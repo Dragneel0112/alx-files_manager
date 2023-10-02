7536514f4bd71c324872f83e6d1a41218b5957e8
Dragneel0112 <pillayruven@gmail.com>
2023-10-02 09:54:50 +0000

    Added files for Task 3

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+
+  api.post('/users', UsersController.postNew);
 };
 
 export default injectRoutes;
