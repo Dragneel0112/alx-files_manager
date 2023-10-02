3b18a72eeb656887dec4ac57327c4361b285746e
Dragneel0112 <pillayruven@gmail.com>
2023-10-02 12:00:18 +0000

    Updated files for Task 8

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -10,6 +10,7 @@ import {
 import { join as joinPath } from 'path';
 import { Request, Response } from 'express';
 import mongoDBCore from 'mongodb/lib/core';
+import { contentType } from 'mime-types';
 import dbClient from '../utils/db';
 import { getUserFromXToken } from '../utils/auth';
 
@@ -253,5 +254,48 @@ export default class FilesController {
         ? 0
         : file.parentId.toString(),
     });
+  }
+
+  /**
+   * Retrieves the content of a file
+   * @param {Request} req - The Express request object.
+   * @param {Response} res - The Express response object.
+   */
+  static async getFile(req, res) {
+    const user = await getUserFromXToken(req);
+    const { id } = req.params;
+    const size = req.query.size || null;
+    const userId = user ? user._id.toString() : '';
+    const fileFilter = {
+      _id: new mongoDBCore.BSON.ObjectId(isValidId(id) ? id : NULL_ID),
+    };
+    const file = await (await dbClient.filesCollection())
+      .findOne(fileFilter);
+
+    if (!file || (!file.isPublic && (file.userId.toString() !== userId))) {
+      res.status(404).json({ error: 'Not found' });
+      return;
+    }
+    if (file.type === VALID_FILE_TYPES.folder) {
+      res.status(400).json({ error: 'A folder doesn\'t have content' });
+      return;
+    }
+    let filePath = file.localPath;
+    if (size) {
+      filePath = `${file.localPath}_${size}`;
+    }
+    if (existsSync(filePath)) {
+      const fileInfo = await statAsync(filePath);
+      if (!fileInfo.isFile()) {
+        res.status(404).json({ error: 'Not found' });
+        return;
+      }
+    } else {
+      res.status(404).json({ error: 'Not found' });
+      return;
+    }
+    const absoluteFilePath = await realpathAsync(filePath);
+    res.setHeader('Content-Type', contentType(file.name) || 'text/plain; charset=utf-8');
+    res.status(200).sendFile(absoluteFilePath);
   }
 }
