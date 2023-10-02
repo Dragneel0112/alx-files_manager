12c7014b865d7a3de543e0080a52888bbb3a9bc7
Dragneel0112 <pillayruven@gmail.com>
2023-10-02 10:23:32 +0000

    Added files for Task 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -31,4 +31,10 @@ export default class UsersController {
     userQueue.add({ userId });
     res.status(201).json({ email, id: userId });
   }
+
+  static async getMe(req, res) {
+    const { user } = req;
+
+    res.status(200).json({ email: user.email, id: user._id.toString() });
+  }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import { basicAuthenticate, xTokenAuthenticate } from '../interface/auth';
 
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
 
   api.post('/users', UsersController.postNew);
+
+  api.get('/connect', basicAuthenticate, AuthController.getConnect);
+  api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
+  api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
 };
 
 export default injectRoutes;
