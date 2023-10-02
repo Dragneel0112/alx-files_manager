ba5b9671aedd928efe783df12eda53310a1cc718
Dragneel0112 <pillayruven@gmail.com>
2023-10-02 09:26:08 +0000

    Updated files fixed eslint

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
  * Server with express
  */
 import express from 'express';
-import startServer from './libs/boot';
+import startServer from './library/boot';
 import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
+import injectMiddlewares from './library/middlewares';
 
 const server = express();
 
